a6564bdf51a44c3b40b4f0182cf3c5399a7a5cf6
Bill_Yang <ym13883407586@outlook.com>
2018-03-17 00:29:32 +0800

    update code

--- SCOI2014-onlinejudge.cpp
+++ SCOI2014-onlinejudge.cpp
@@ -10,11 +10,13 @@ inline int Get_Int() {
 	return num*bj;
 }
 
+const int L=-2e8,R=2e8; //boundary limit
+
 struct Tree {
 	Tree *ls,*rs;
 	int size,val;
 	Tree(int l=0,int r=0) {size=r-l+1;}
-} *root=Tree(-2e8,2e8);
+} *root=new Tree(L,R);
 
 struct Segment_Tree {
 #define mid ((left+right)>>1)
@@ -22,24 +24,24 @@ struct Segment_Tree {
 	void insert(Tree *&now,int left,int right,int tar,int val) {
 		newnode(now,left,right);
 		if(left==right) {now->val=val;return;}
-		newnode(ls,left,mid),newnode(rs,mid+1,right);
-		if(tar<=ls->size)insert(now->ls,left,mid,tar,val);
-		else insert(now->rs,mid+1,right,tar-ls->size,val);
+		newnode(now->ls,left,mid),newnode(now->rs,mid+1,right);
+		if(tar<=now->ls->size)insert(now->ls,left,mid,tar,val);
+		else insert(now->rs,mid+1,right,tar-now->ls->size,val);
 	}
 	void remove(Tree *&now,int left,int right,int tar) {
 		if(!now)now=new Tree(left,right);
 		now->size--;
 		if(left==right)return;
-		newnode(ls,left,mid),newnode(rs,mid+1,right);
-		if(tar<=ls->size)remove(now->ls,left,mid,tar);
-		else remove(now->rs,mid+1,right,tar-ls->size);
+		newnode(now->ls,left,mid),newnode(now->rs,mid+1,right);
+		if(tar<=now->ls->size)remove(now->ls,left,mid,tar);
+		else remove(now->rs,mid+1,right,tar-now->ls->size);
 	}
 	int query(Tree *&now,int left,int right,int tar) {
 		if(!now)now=new Tree(left,right);
 		if(left==right)return now->val?now->val:left;
-		newnode(ls,left,mid),newnode(rs,mid+1,right);
-		if(tar<=ls->size)return query(now->ls,left,mid,tar);
-		else return query(now->rs,mid+1,right,tar-ls->size);
+		newnode(now->ls,left,mid),newnode(now->rs,mid+1,right);
+		if(tar<=now->ls->size)return query(now->ls,left,mid,tar);
+		else return query(now->rs,mid+1,right,tar-now->ls->size);
 	}
 	int sum(Tree *&now,int left,int right,int Left,int Right) {
 		if(Right<left||Left>right)return 0;
@@ -49,16 +51,37 @@ struct Segment_Tree {
 	}
 } st;
 
+map<int,int> id;
+int n,m,ans=0;
+
 int main() {
 	n=Get_Int();
 	m=Get_Int();
-	int L=-2e8,R=2e8;
+	int left=1,right=n;
 	for(int i=1; i<=m; i++) {
 		int opt=Get_Int();
 		if(opt==1) {
 			int x=Get_Int()-ans,y=Get_Int()-ans;
 			id[y]=id.count(x)?id[x]:x;
-			st.insert(root,L,R,left-L+st.sum(root,L,R,left,id[y]),y);
+			st.insert(root,L,R,st.sum(root,L,R,L,id[y]),y); //insert at id[y]
+			printf("%d\n",ans=st.sum(root,L,R,left,id[y]));
+		} else if(opt==2) {
+			int x=Get_Int()-ans;
+			ans=st.sum(root,L,R,left,id.count(x)?id[x]:x);
+			printf("%d\n",ans);
+			st.remove(root,L,R,left-L+ans);
+			st.insert(root,L,R,left-L,x);
+			id[x]=--left;
+		} else if(opt==3) {
+			int x=Get_Int()-ans;
+			ans=st.sum(root,L,R,left,id.count(x)?id[x]:x);
+			printf("%d\n",ans);
+			st.remove(root,L,R,left-L+ans);
+			st.insert(root,L,R,st.sum(root,L,R,L,right)+1,x);
+			id[x]=++right;
+		} else {
+			int k=Get_Int()-ans;
+			printf("%d\n",ans=st.query(root,L,R,left-L+k));
 		}
 	}
 	return 0;
