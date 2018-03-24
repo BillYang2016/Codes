ab37a00e6711c11e3cbe9ebbf6e7cbe026de05e9
Bill_Yang <ym13883407586@outlook.com>
2018-03-24 20:25:48 +0800

    added codes

--- CQOI2014-matrix-2.cpp
+++ CQOI2014-matrix-2.cpp
@@ -10,11 +10,9 @@ inline int Get_Int() {
 	return num*bj;
 }
 
-const int dx[]= {0,1,-1,0,0},dy[]= {0,0,0,1,-1};
-
 int n,m;
-bool ans[1605];
-bitset<1605> a[1605];
+bool ans[45][45];
+bitset<45> a[45],b[45][45];
 
 void Gauss(int n) {
 	for(int i=1; i<=n; i++) {
@@ -25,25 +23,24 @@ void Gauss(int n) {
 		for(int j=i+1; j<=n; j++)if(a[j][i])a[j]^=a[i];
 	}
 	for(int i=n; i>=1; i--) {
-		ans[i]=a[i][i]?a[i][n+1]:1;
-		if(ans[i])for(int j=1; j<i; j++)if(a[j][i])a[j].flip(n+1);
+		ans[1][i]=a[i][i]?a[i][n+1]:1;
+		if(ans[1][i])for(int j=1; j<i; j++)if(a[j][i])a[j].flip(n+1);
 	}
 }
-
-int id(int x,int y) {return (x-1)*m+y;}
 
 int main() {
 	n=Get_Int();
 	m=Get_Int();
-	for(int i=1; i<=n; i++)
+	for(int i=1; i<=m; i++)b[1][i][i]=1;
+	for(int i=2; i<=n+1; i++)
 		for(int j=1; j<=m; j++)
-			for(int k=0; k<=4; k++) {
-				int nx=i+dx[k],ny=j+dy[k];
-				if(nx<1||ny<1||nx>n||ny>m)continue;
-				a[id(i,j)][id(nx,ny)]=1;
-			}
-	Gauss(n*m);
+			b[i][j]=b[i-1][j-1]^b[i-1][j]^b[i-1][j+1]^b[i-2][j];
+	for(int i=1; i<=m; i++)a[i]=b[n+1][i];
+	Gauss(m);
+	for(int i=2; i<=n; i++)
+		for(int j=1; j<=m; j++)
+			ans[i][j]=ans[i-1][j-1]^ans[i-1][j]^ans[i-1][j+1]^ans[i-2][j];
 	for(int i=1; i<=n; i++)
-		for(int j=1; j<=m; j++)printf("%d%c",ans[id(i,j)],j==m?'\n':' ');
+		for(int j=1; j<=m; j++)printf("%d%c",ans[i][j],j==m?'\n':' ');
 	return 0;
 }
