8812b8f5fe301046590e7578037f4b9f3436ece3
Bill_Yang_2016 <ym13883407586@outlook.com>
2018-04-06 19:06:50 +0800

    added codes

---
+++ 51nod1577-xor.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+inline int Get_Int() {
+	int num=0,bj=1;
+	char x=getchar();
+	while(!isdigit(x)) {if(x=='-')bj=-1;x=getchar();}
+	while(isdigit(x)) {num=num*10+x-'0';x=getchar();}
+	return num*bj;
+}
+
+const int maxn=500005,K=29;
+
+struct node {
+	int val,id;
+	node(int a=0,int b=0):val(a),id(b) {}
+};
+
+struct Linear_Bases {
+	node b[K+1];
+	Linear_Bases() {fill(b,b+K+1,0);}
+	node& operator [] (const int x) {return b[x];}
+	void add(node now) {
+		for(int j=K; j>=0; j--)
+			if(now.val>>j&1) {
+				if(b[j].val) {
+					swap(b[j],now);
+					now.val^=b[j].val;
+					continue;
+				}
+				b[j]=now;
+				break;
+			}
+	}
+} f[maxn];
+
+int n,m,a[maxn];
+
+int main() {
+	n=Get_Int();
+	for(int i=1; i<=n; i++)a[i]=Get_Int();
+	for(int i=n; i>=1; i--) {
+		f[i]=f[i+1];
+		f[i].add(node(a[i],i));
+	}
+	m=Get_Int();
+	while(m--) {
+		int l=Get_Int(),r=Get_Int(),k=Get_Int();
+		for(int j=K; j>=0; j--) {
+			if(!(k>>j&1))continue;
+			if(!f[l][j].val||f[l][j].id>r)break;
+			k^=f[l][j].val;
+		}
+		puts(k?"NO":"YES");
+	}
+	return 0;
+}
---
+++ bsoj4246-xor.cpp
@@ -0,0 +1,102 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef unsigned int LL;
+
+inline LL Get_Int() {
+	LL num=0,bj=1;
+	char x=getchar();
+	while(!isdigit(x)) {if(x=='-')bj=-1;x=getchar();}
+	while(isdigit(x)) {num=num*10+x-'0';x=getchar();}
+	return num*bj;
+}
+
+const int maxn=20005,K=31;
+
+struct Linear_Bases {
+	vector<LL> a;
+	LL b[K+1];
+	Linear_Bases() {fill(b,b+K+1,0);}
+	void add(LL num) {
+		for(int j=K; j>=0; j--)
+			if(num>>j&1) {
+				if(b[j]) {num^=b[j];continue;}
+				b[j]=num;
+				for(int k=j-1; k>=0; k--)if(b[j]>>k&1)b[j]^=b[k];
+				for(int k=j+1; k<=K; k++)if(b[k]>>j&1)b[k]^=b[j];
+				break;
+			}
+	}
+	void split() {for(int i=K; i>=0; i--)if(b[i])a.push_back(b[i]);}
+	void merge(const Linear_Bases& b) {for(int i=0; i<=K; i++)if(b.b[i])add(b.b[i]);}
+	LL cal() {
+		LL ans=0;
+		for(int i=0; i<=K; i++)ans^=b[i];
+		return ans;
+	}
+} null;
+
+int n,q;
+LL a[maxn];
+
+struct Segment_Tree {
+	struct Tree {
+		int left,right;
+		Linear_Bases lb;
+	} tree[maxn<<2];
+#define ls index<<1
+#define rs index<<1|1
+	void build(int index,int Left,int Right) {
+		tree[index].left=Left,tree[index].right=Right;
+		if(Left==Right) {tree[index].lb.add(a[Left]);return;}
+		int mid=(Left+Right)>>1;
+		build(ls,Left,mid);
+		build(rs,mid+1,Right);
+		push_up(index);
+	}
+	void push_up(int index) {
+		tree[index].lb=tree[ls].lb;
+		tree[index].lb.merge(tree[rs].lb);
+	}
+	void modify(int index,int tar,LL val) {
+		if(tar<tree[index].left||tar>tree[index].right)return;
+		if(tree[index].left==tree[index].right) {
+			tree[index].lb=null;
+			tree[index].lb.add(val);
+			return;
+		}
+		modify(ls,tar,val);
+		modify(rs,tar,val);
+		push_up(index);
+	}
+	Linear_Bases query(int index,int Left,int Right) {
+		if(Right<tree[index].left||Left>tree[index].right)return null;
+		if(Left<=tree[index].left&&Right>=tree[index].right)return tree[index].lb;
+		Linear_Bases l=query(ls,Left,Right),r=query(rs,Left,Right);
+		l.merge(r);
+		return l;
+	}
+} st;
+
+void Query(int Left,int Right) {
+	Linear_Bases lb=st.query(1,Left,Right);
+	lb.split();
+	int len=lb.a.size();
+	LL Max=lb.cal(),ans=0;
+	for(int i=1; i<lb.a.size(); i++)ans+=lb.a[i]^Max;
+	printf("%u\n",ans+Max*((Right-Left+1)-len+1));
+}
+
+int main() {
+	n=Get_Int();
+	q=Get_Int();
+	for(int i=1; i<=n; i++)a[i]=Get_Int();
+	st.build(1,1,n);
+	while(q--) {
+		LL opt=Get_Int(),x=Get_Int(),y=Get_Int();
+		if(opt==1)st.modify(1,x,y);
+		else Query(x,y);
+	}
+	return 0;
+}
