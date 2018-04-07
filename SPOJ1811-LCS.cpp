5278921538c0a4c220e8fad971f316e2a99b7d0c
Bill_Yang_2016 <ym13883407586@outlook.com>
2018-04-07 20:48:13 +0800

    added code

--- SPOJ1811-LCS.cpp
+++ SPOJ1811-LCS.cpp
@@ -15,15 +15,8 @@ const int maxn=250005,maxc=26;
 struct Suffix_Automaton {
 	int cnt,root,last;
 	int child[maxn<<1][maxc],next[maxn<<1],Max[maxn<<1];
-	Suffix_Automaton() {init();}
-	void init() {cnt=0;root=last=newnode(0);}
-	int newnode(int val) {
-		cnt++;
-		next[cnt]=0;
-		Max[cnt]=val;
-		fill(child[cnt],child[cnt]+maxc,0);
-		return cnt;
-	}
+	Suffix_Automaton() {cnt=0;root=last=newnode(0);}
+	int newnode(int val) {Max[++cnt]=val;return cnt;}
 	void insert(int data) {
 		int p=last,u=newnode(Max[last]+1);
 		last=u;
@@ -41,18 +34,16 @@ struct Suffix_Automaton {
 			}
 		}
 	}
-	void build(string s) {
-		for(auto x:s)insert(x-'a');
-	}
+	void build(string s) {for(auto x:s)insert(x-'a');}
 	int run(string s) {
-		int ans=0,len=0,p=root;
-		for(int x:s) {
+		int p=root,len=0,ans=0;
+		for(auto x:s) {
 			int ch=x-'a';
-			if(child[p][ch])p=child[p][ch],len++;
+			if(child[p][ch])len++,p=child[p][ch];
 			else {
 				while(p&&!child[p][ch])p=next[p];
-				if(!p) {len=0,p=root;}
-				else {len=Max[p]+1,p=child[p][ch];}
+				if(!p)len=0,p=root;
+				else len=Max[p]+1,p=child[p][ch];
 			}
 			ans=max(ans,len);
 		}
