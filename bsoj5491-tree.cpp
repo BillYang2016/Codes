fb75619eec9421f4affcec10304fe5f2b6186209
Bill_Yang_2016 <ym13883407586@outlook.com>
2018-04-11 16:19:57 +0800

    added code

--- bsoj5491-tree.cpp
+++ bsoj5491-tree.cpp
@@ -10,7 +10,17 @@ inline int Get_Int() {
 	return num*bj;
 }
 
+const int mod=2007;
+
+int Pow[105];
+
 int main() {
-	
+	Pow[0]=1;
+	for(int i=1; i<=100; i++)Pow[i]=Pow[i-1]*5%mod;
+	int t=Get_Int();
+	while(t--) {
+		int n=Get_Int();
+		printf("%d\n",4*n%mod*Pow[n-1]%mod);
+	}
 	return 0;
 }
