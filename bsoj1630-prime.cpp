08ce9076fb52863d6373b5e1a254ac0dee1a1034
Bill_Yang_2016 <ym13883407586@outlook.com>
2018-04-12 15:24:54 +0800

    added code

--- bsoj1630-prime.cpp
+++ bsoj1630-prime.cpp
@@ -1,42 +1,27 @@
-#include<algorithm>
-#include<iostream>
-#include<iomanip>
-#include<cstring>
-#include<cstdlib>
-#include<climits>
-#include<vector>
-#include<cstdio>
-#include<cmath>
-#include<queue>
+#include<bits/stdc++.h>
+
 using namespace std;
 
 typedef long long LL;
 
-inline const LL Get_Int() {
+inline LL Get_Int() {
 	LL num=0,bj=1;
 	char x=getchar();
-	while(x<'0'||x>'9') {
-		if(x=='-')bj=-1;
-		x=getchar();
-	}
-	while(x>='0'&&x<='9') {
-		num=num*10+x-'0';
-		x=getchar();
-	}
+	while(!isdigit(x)) {if(x=='-')bj=-1;x=getchar();}
+	while(isdigit(x)) {num=num*10+x-'0';x=getchar();}
 	return num*bj;
 }
 
-const int TIMES=10;
+const int times=10;
 
-LL Quick_Mul(LL a,LL b,LL p) {
+LL Mul(LL a,LL b,LL p) {
 	LL tmp=(a*b-(LL)((long double)a/p*b+1e-8)*p);
-	if(tmp<0)return tmp+p;
-	else return tmp;
+	return tmp<0?tmp+p:tmp;
 }
 
 LL Quick_Pow(LL a,LL b,LL p) {
 	LL sum=1;
-	for(a%=p; b; b>>=1,a=Quick_Mul(a,a,p))if(b&1)sum=Quick_Mul(sum,a,p);
+	for(a%=p; b; b>>=1,a=Mul(a,a,p))if(b&1)sum=Mul(sum,a,p);
 	return sum;
 }
 
@@ -46,29 +31,26 @@ bool Witness(LL a,LL n) {
 	LL u=n-1,t=0;
 	while(u%2==0)t++,u>>=1;
 	LL x=Quick_Pow(a,u,n);
-	if(x==1)return false;
-	for(int i=1; i<=t; i++,x=Quick_Mul(x,x,n))
-		if(x!=n-1&&x!=1&&Quick_Mul(x,x,n)==1)return true;
+	for(int i=1; i<=t&&x!=1; i++,x=Mul(x,x,n))if(x!=n-1&&Mul(x,x,n)==1)return true;
 	return x!=1;
 }
 
 bool Miller_Rabin(LL n) {
 	if(n==2)return true;
 	if(n<2||!(n&1))return false;
-	for(int i=1; i<=TIMES; i++)
-		if(Witness(g()%(n-1)+1,n))return false;
+	for(int i=1; i<=times; i++)if(Witness(g()%(n-1)+1,n))return false;
 	return true;
 }
 
-LL Pollar_Rho(LL n) {
+LL Pollard_Rho(LL n) {
 	if(!(n&1))return 2;
 	while(true) {
 		LL a=g()%(n-1)+1,b=a,c=g()%(n-1)+1;
 		if(c==2)c=3;
 		while(true) {
-			a=(Quick_Mul(a,a,n)+c)%n;
-			b=(Quick_Mul(b,b,n)+c)%n;
-			b=(Quick_Mul(b,b,n)+c)%n;
+			a=(Mul(a,a,n)+c)%n;
+			b=(Mul(b,b,n)+c)%n;
+			b=(Mul(b,b,n)+c)%n;
 			if(a==b)break; //环
 			LL d=__gcd(abs(b-a),n);
 			if(d>1)return d;
@@ -76,10 +58,10 @@ LL Pollar_Rho(LL n) {
 	}
 }
 
-LL Find_Frac(LL n) {
+LL Find_Fac(LL n) {
 	if(Miller_Rabin(n))return n;
-	LL p=Pollar_Rho(n);
-	return min(Find_Frac(p),Find_Frac(n/p));
+	LL p=Pollard_Rho(n);
+	return min(Find_Fac(p),Find_Fac(n/p));
 }
 
 LL n;
@@ -89,9 +71,9 @@ int main() {
 	int t=Get_Int();
 	while(t--) {
 		n=Get_Int();
-		LL ans=Find_Frac(n);
+		LL ans=Find_Fac(n);
 		if(ans==n)puts("Prime");
 		else printf("%lld\n",ans);
 	}
 	return 0;
-} 
+}
