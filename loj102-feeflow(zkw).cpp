3f72697acbe5059ce4e316bd25e9036e71c57084
Bill_Yang_2016 <ym13883407586@outlook.com>
2018-04-13 09:35:20 +0800

    update

--- loj102-feeflow(zkw).cpp
+++ loj102-feeflow(zkw).cpp
@@ -1,47 +1,32 @@
-#include<algorithm>
-#include<iostream>
-#include<iomanip>
-#include<cstring>
-#include<cstdlib>
-#include<climits>
-#include<vector>
-#include<cstdio>
-#include<cmath>
-#include<queue>
+#include<bits/stdc++.h>
+
 using namespace std;
 
-inline const int Get_Int() {
+inline int Get_Int() {
 	int num=0,bj=1;
 	char x=getchar();
-	while(x<'0'||x>'9') {
-		if(x=='-')bj=-1;
-		x=getchar();
-	}
-	while(x>='0'&&x<='9') {
-		num=num*10+x-'0';
-		x=getchar();
-	}
+	while(!isdigit(x)) {if(x=='-')bj=-1;x=getchar();}
+	while(isdigit(x)) {num=num*10+x-'0';x=getchar();}
 	return num*bj;
 }
 
-const int maxn=405;
+const int maxn=405; 
 
 struct Edge {
 	int from,to,cap,flow,cost;
-	Edge(int x=0,int y=0,int c=0,int f=0,int co=0):from(x),to(y),cap(c),flow(f),cost(co) {}
+	Edge(int x=0,int y=0,int c=0,int f=0,int v=0):from(x),to(y),cap(c),flow(f),cost(v) {}
 };
 
 struct zkw_CostFlow {
 	int n,m,s,t;
-	vector<Edge>edges;
-	vector<int>G[maxn];
-	bool inque[maxn],vst[maxn];
-	int a[maxn],dist[maxn],path[maxn];
-	int flow,cost;
+	vector<Edge> edges;
+	vector<int> G[maxn];
+	int dist[maxn],flow,cost;
+	bool vst[maxn],inque[maxn];
 	void init(int n) {
 		this->n=n;
-		edges.clear();
-		for(int i=1; i<=n; i++)G[i].clear();
+		// edges.clear();
+		// for(int i=1; i<=n; i++)G[i].clear();
 	}
 	void AddEdge(int x,int y,int v,int f) {
 		edges.push_back(Edge(x,y,v,0,f));
@@ -51,30 +36,30 @@ struct zkw_CostFlow {
 		G[y].push_back(m-1);
 	}
 	bool spfa() {
-		for(int i=1; i<=n; i++)dist[i]=INT_MAX;
-		memset(inque,0,sizeof(inque));
+		fill(inque+1,inque+n+1,0);
+		fill(dist+1,dist+n+1,INT_MAX);
 		dist[t]=0;
 		inque[t]=1;
-		deque<int>Q;
+		deque<int> Q;
 		Q.push_back(t);
 		while(!Q.empty()) {
 			int Now=Q.front();
 			Q.pop_front();
 			inque[Now]=0;
 			for(int id:G[Now]) {
-				Edge& e=edges[id^1];
+				Edge &e=edges[id^1];
 				int Next=e.from;
 				if(dist[Next]>dist[Now]+e.cost&&e.cap>e.flow) {
 					dist[Next]=dist[Now]+e.cost;
 					if(!inque[Next]) {
 						inque[Next]=1;
-						if(!Q.empty()&&dist[Next]<dist[Q.front()])Q.push_front(Next);
+						if(!Q.empty()&&dist[Q.front()]>dist[Next])Q.push_front(Next);
 						else Q.push_back(Next);
 					}
 				}
 			}
 		}
-		return dist[s]<INT_MAX;
+		return dist[s]!=INT_MAX;
 	}
 	int dfs(int Now,int a) {
 		if(vst[Now])return 0;
@@ -82,11 +67,11 @@ struct zkw_CostFlow {
 		int flow=0;
 		vst[Now]=1;
 		for(int id:G[Now]) {
-			Edge& e=edges[id];
+			Edge &e=edges[id];
 			int Next=e.to;
-			if(dist[Now]-e.cost!=dist[Next])continue;
+			if(dist[Next]!=dist[Now]-e.cost)continue;
 			int nextflow=dfs(Next,min(a,e.cap-e.flow));
-			if(nextflow>0) {
+			if(nextflow) {
 				cost+=nextflow*e.cost;
 				e.flow+=nextflow;
 				edges[id^1].flow-=nextflow;
@@ -119,8 +104,7 @@ int main() {
 		int x=Get_Int(),y=Get_Int(),v=Get_Int(),f=Get_Int();
 		zkw.AddEdge(x,y,v,f);
 	}
-	int flow=0;
-	flow=zkw.maxflow(1,n);
+	int flow=zkw.maxflow(1,n);
 	printf("%d %d\n",flow,zkw.cost);
 	return 0;
 }
