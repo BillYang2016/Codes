a92d537bdea96f6fd2fbdaa7ca6d246028b603c4
Bill_Yang_2016 <ym13883407586@outlook.com>
2018-04-13 08:58:52 +0800

    speed up

--- loj101-maxflow.cpp
+++ loj101-maxflow.cpp
@@ -1,26 +1,12 @@
-#include<algorithm>
-#include<iostream>
-#include<iomanip>
-#include<cstring>
-#include<cstdlib>
-#include<climits>
-#include<vector>
-#include<cstdio>
-#include<cmath>
-#include<queue>
+#include<bits/stdc++.h>
+
 using namespace std;
 
-inline const int Get_Int() {
+inline int Get_Int() {
 	int num=0,bj=1;
 	char x=getchar();
-	while(x<'0'||x>'9') {
-		if(x=='-')bj=-1;
-		x=getchar();
-	}
-	while(x>='0'&&x<='9') {
-		num=num*10+x-'0';
-		x=getchar();
-	}
+	while(!isdigit(x)) {if(x=='-')bj=-1;x=getchar();}
+	while(isdigit(x)) {num=num*10+x-'0';x=getchar();}
 	return num*bj;
 }
 
@@ -28,19 +14,19 @@ const int maxn=1000005;
 
 struct Edge {
 	int from,to,cap,flow;
-	Edge(int x=0,int y=0,int c=0,int f=0):from(x),to(y),cap(c),flow(f) {}
+	Edge(int x=0,int y=0,int v=0,int f=0):from(x),to(y),cap(v),flow(f) {}
 };
 
 struct Dinic {
 	int n,m,s,t;
-	vector<Edge>edges;
-	vector<int>G[maxn];
-	bool vst[maxn];
+	vector<Edge> edges;
+	vector<int> G[maxn];
 	int dist[maxn],cur[maxn];
+	bool vst[maxn];
 	void init(int n) {
 		this->n=n;
-		edges.clear();
-		for(int i=1; i<=n; i++)G[i].clear();
+		// edges.clear();
+		// for(int i=1; i<=n; i++)G[i].clear();
 	}
 	void AddEdge(int x,int y,int v) {
 		edges.push_back(Edge(x,y,v,0));
@@ -50,20 +36,20 @@ struct Dinic {
 		G[y].push_back(m-1);
 	}
 	bool bfs() {
-		memset(vst,0,sizeof(vst));
-		memset(dist,0,sizeof(dist));
-		queue<int>Q;
-		Q.push(t); //反向层次图
+		fill(vst+1,vst+n+1,0);
+		queue<int> Q;
+		Q.push(t);
 		vst[t]=1;
 		while(!Q.empty()) {
 			int Now=Q.front();
 			Q.pop();
-			for(int i=0; i<G[Now].size(); i++) {
-				Edge& e=edges[G[Now][i]^1];
+			for(int id:G[Now]) {
+				Edge e=edges[id^1];
 				int Next=e.from;
-				if(!vst[Next]&&e.cap>e.flow) { 
+				if(!vst[Next]&&e.cap>e.flow) {
 					vst[Next]=1;
 					dist[Next]=dist[Now]+1;
+					if(Next==s)return 1;
 					Q.push(Next);
 				}
 			}
@@ -73,12 +59,12 @@ struct Dinic {
 	int dfs(int Now,int a) {
 		if(Now==t||a==0)return a;
 		int flow=0;
-		for(int& i=cur[Now]; i<G[Now].size(); i++) {
-			Edge& e=edges[G[Now][i]];
+		for(int &i=cur[Now]; i<G[Now].size(); i++) {
+			Edge &e=edges[G[Now][i]];
 			int Next=e.to;
-			if(dist[Now]-1!=dist[Next])continue;
+			if(dist[Next]!=dist[Now]-1)continue;
 			int nextflow=dfs(Next,min(a,e.cap-e.flow));
-			if(nextflow>0) {
+			if(nextflow) {
 				e.flow+=nextflow;
 				edges[G[Now][i]^1].flow-=nextflow;
 				flow+=nextflow;
