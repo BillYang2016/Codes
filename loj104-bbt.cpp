26515aa441ba3e087e0a76e0721460cdc9bf640f
Bill_Yang_2016 <ym13883407586@outlook.com>
2018-04-14 19:08:24 +0800

    modified template

---
+++ loj104-bbt.cpp
@@ -0,0 +1,141 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+inline int Get_Int() {
+	int num=0,bj=1;
+	char x=getchar();
+	while(!isdigit(x)) {if(x=='-')bj=-1;x=getchar();}
+	while(isdigit(x)) {num=num*10+x-'0';x=getchar();}
+	return num*bj;
+}
+
+const int maxn=100005;
+
+mt19937 g(rand());
+
+struct Treap {
+	struct Tree {
+		int child[2],size;
+		int d,val;
+	} tree[maxn];
+	int size,root;
+	queue<int> Q;
+#define d(x) tree[x].d
+#define val(x) tree[x].val
+#define ls(x) tree[x].child[0]
+#define rs(x) tree[x].child[1]
+#define size(x) tree[x].size 
+	void push_up(int x) {size(x)=size(ls(x))+size(rs(x))+1;}
+	int newnode(int val) {
+		int now;
+		if(!Q.empty()) {
+			now=Q.front();
+			Q.pop();
+			ls(now)=rs(now)=0;
+		} else now=++size;
+		val(now)=val;
+		d(now)=g()%maxn;
+		size(now)=1;
+		return now;
+	}
+	void rotate(int &x,bool side) {
+		int y=tree[x].child[!side];
+		tree[x].child[!side]=tree[y].child[side];
+		tree[y].child[side]=x;
+		push_up(x),push_up(y);
+		x=y;
+	}
+	void insert(int &x,int v) {
+		if(!x) {x=newnode(v);return;}
+		bool side=v>val(x);
+		int &y=tree[x].child[side];
+		insert(y,v);
+		size(x)++;
+		if(d(y)>d(x))rotate(x,!side);
+	}
+	int cnt,a[maxn];
+	void dfs(int x) {
+		if(!x)return;
+		dfs(ls(x));
+		a[++cnt]=val(x),Q.push(x);
+		dfs(rs(x));
+	}
+	int build(int l,int r) {
+		if(l>r)return 0;
+		int mid=(l+r)>>1,now=newnode(a[mid]);
+		ls(now)=build(l,mid-1);
+		rs(now)=build(mid+1,r);
+		push_up(now);
+		return now;
+	}
+	int rebuild(int x) {
+		cnt=0;
+		dfs(ls(x));
+		dfs(rs(x));
+		return build(1,cnt);
+	}
+	void remove(int val) {
+		int now=root,fa;
+		bool side;
+		while(val(now)!=val) {
+			fa=now;
+			size(now)--;
+			if(val<val(now))now=ls(now),side=0;
+			else now=rs(now),side=1;
+		}
+		int pos=rebuild(now);
+		if(now==root)root=pos;
+		else tree[fa].child[side]=pos;
+	}
+	int rank(int val) {
+		int now=root,ans=0;
+		while(now) {
+			if(val<=val(now))now=ls(now);
+			else ans+=size(ls(now))+1,now=rs(now);
+		}
+		return ans+1;
+	}
+	int kth(int rank) {
+		int now=root;
+		while(now&&rank) {
+			if(rank<=size(ls(now)))now=ls(now);
+			else {
+				if(rank==size(ls(now))+1)return now;
+				rank-=size(ls(now))+1;
+				now=rs(now);
+			}
+		}
+		return -1;
+	}
+	int pre(int val) {
+		int now=root,ans=INT_MIN;
+		while(now) {
+			if(val(now)<val)ans=max(ans,val(now)),now=rs(now);
+			else now=ls(now);
+		}
+		return ans;
+	}
+	int suc(int val) {
+		int now=root,ans=INT_MAX;
+		while(now) {
+			if(val(now)>val)ans=min(ans,val(now)),now=ls(now);
+			else now=rs(now);
+		}
+		return ans;
+	}
+} treap;
+
+int main() {
+	int n=Get_Int();
+	for(int i=1; i<=n; i++) {
+		int opt=Get_Int();
+		if(opt==1)treap.insert(treap.root,Get_Int());
+		if(opt==2)treap.remove(Get_Int());
+		if(opt==3)printf("%d\n",treap.rank(Get_Int()));
+		if(opt==4)printf("%d\n",treap.tree[treap.kth(Get_Int())].val);
+		if(opt==5)printf("%d\n",treap.pre(Get_Int()));
+		if(opt==6)printf("%d\n",treap.suc(Get_Int()));
+	}
+	return 0;
+}
