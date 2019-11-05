0e5f435e24273a1494a950139ea91cbd2007ac08
Bill Yang <ym13883407586@outlook.com>
2019-11-05 18:58:07 +0800

    add hello-world

---
+++ hello-world.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class ByeWorld {
+	public:
+	ByeWorld() {cout<<"Hello";}
+	~ByeWorld() {cout<<"world!";}
+} Bill_Yang;
+
+int main() {
+	cout<<" ";
+	return 0;
+}
